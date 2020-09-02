6ef64d4325377155a1f50c67b4b996a902f8258b
2112986 <54841736+2112986@users.noreply.github.com>
2020-09-02 15:30:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// oefening 1
+$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"]; 
+
+// oefening 2
+$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"]; 
+
+for($i=0; $i<=3; $i++){
+    echo $klas[$i];
+    echo PHP_EOL;
+}
+
+// oefening 3
+$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"]; 
+
+foreach($klas as $value) {
+  print $value;
+  print PHP_EOL;
+}
+
+// combinatie opdracht
+
+$bucket_list = [];
+$hoeveelheid_activiteiten = [];
+
+
+
+
